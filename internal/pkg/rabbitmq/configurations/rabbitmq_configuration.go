8b9934c7d3450fce311825028354e82bcd7e24df
SniperXDev <abuntrock3@gmail.com>
2024-09-28 04:14:24 -0400

    Refactor usability tests.
    Co-authored-by: SniperXDev <abuntrock3@gmail.com>
    Co-authored-by: OpenBitSack <daniellucki07@gmail.com>

--- internal/pkg/rabbitmq/configurations/rabbitmq_configuration.go
+++ internal/pkg/rabbitmq/configurations/rabbitmq_configuration.go
@@ -5,6 +5,7 @@ import (
 	producerConfigurations "github.com/mehdihadeli/go-food-delivery-microservices/internal/pkg/rabbitmq/producer/configurations"
 )
 
+// author SniperXDev
 type RabbitMQConfiguration struct {
 	ProducersConfigurations []*producerConfigurations.RabbitMQProducerConfiguration
 	ConsumersConfigurations []*consumerConfigurations.RabbitMQConsumerConfiguration
